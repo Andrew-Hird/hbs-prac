bead135a3cefa32d68009593ed5885b83e1b7001
EDA <eda@mac-mini-1.local>
2016-09-06 15:15:53 +1200

    index async

--- server.js
+++ server.js
@@ -2,30 +2,27 @@ var express = require('express')
 var hbs = require('express-handlebars')
 var fs = require('fs')
 var app = express()
-
-
-
-// var weather = require('./weather')
 var path = require('path')
 
-app.engine ('hbs', hbs())
+app.engine('hbs', hbs())
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get ('/', function (req, res) {
-  res.render('index', weather)
+app.get('/', function (req, res) {
+  fs.readFile('./weather.json', function (err, json) {
+    weather = JSON.parse(json)
+    res.render('index', weather)
+  })
 })
 
-app.get ('/weather/:id', function (req, res) {
-  fs.readFile('./weather.json', function (err, json){
+app.get('/weather/:id', function (req, res) {
+  fs.readFile('./weather.json', function (err, json) {
     weather = JSON.parse(json)
     res.render('weather', weather.weather[req.params.id])
   })
 })
 
-
-
-app.listen (3000, function(){
-   console.log ('listening on port 3000')
+app.listen(3000, function () {
+  console.log('listening on port 3000')
 })
